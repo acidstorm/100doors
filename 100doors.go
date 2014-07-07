02b938de907372c61cea685e81316c0300d517e9
Victor Akujobi <victor.akujobi@gmail.com>
2014-07-08 00:21:55 +0100

    Add 100 Doors implemented in Go

---
+++ 100doors.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+const MAX_DOOR = 100
+
+func main() {
+	Doors := [4]int32{0, 0, 0, 0}
+	for step := 1; step < MAX_DOOR; step++ {
+		for door := 0; door < MAX_DOOR; door += step {
+			if Doors[door>>5]&(1<<(uint(door&31))) != 0 {
+				Doors[door>>5] &= ^(1 << uint(door&31))
+			} else {
+				Doors[door>>5] |= (1 << uint(door&31))
+			}
+
+		}
+	}
+	for door := 1; door < MAX_DOOR; door++ {
+		if Doors[door>>5]&(1<<(uint(door&31))) != 0 {
+			fmt.Printf("\nDoors #%d: is open\n", door)
+		} else {
+			fmt.Printf("\nDoors #%d: is closed\n", door)
+		}
+
+	}
+	// print
+}
