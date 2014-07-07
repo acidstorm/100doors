47bd8a4d4f7cac0284c822d0bf72fbc05f05c55f
Victor Akujobi <victor.akujobi@gmail.com>
2014-07-08 00:35:46 +0100

    Removed unnecessary print statement

--- 100doors.go
+++ 100doors.go
@@ -8,8 +8,8 @@ const MAX_DOOR = 100
 
 func main() {
 	Doors := [4]int32{0, 0, 0, 0}
-	for step := 1; step < MAX_DOOR; step++ {
-		for door := 0; door < MAX_DOOR; door += step {
+	for step := 1; step <= MAX_DOOR; step++ {
+		for door := 0; door <= MAX_DOOR; door += step {
 			if Doors[door>>5]&(1<<(uint(door&31))) != 0 {
 				Doors[door>>5] &= ^(1 << uint(door&31))
 			} else {
@@ -18,12 +18,12 @@ func main() {
 
 		}
 	}
-	for door := 1; door < MAX_DOOR; door++ {
+	for door := 1; door <= MAX_DOOR; door++ {
 		if Doors[door>>5]&(1<<(uint(door&31))) != 0 {
 			fmt.Printf("\nDoors #%d: is open\n", door)
 		} else {
-			fmt.Printf("\nDoors #%d: is closed\n", door)
+			// fmt.Printf("\nDoors #%d: is closed\n", door)
 		}
-
 	}
+	fmt.Printf("\nAll other doors are closed\n")
 }
